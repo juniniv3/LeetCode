d8f74a6e511fa3b24b2fc2a5b82617879683fbb8
Yisus Florez <juniniprogramador@outlook.com>
2024-09-06 17:20:27 -0500

    Time: 1 ms (99.79%), Space: 52.6 MB (69.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,19 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        int majorityElement = nums[0];
-        for (int num : nums) {
-            if (countOccurrences(nums, num) > nums.length / 2) {
-                majorityElement = num;
-                break;
-            }
-        }
-        return majorityElement;
-    }
-
-    public static int countOccurrences(int[] arr, int target) {
+    public static int majorityElement(int[] nums) {
         int count = 0;
-        for (int element : arr) {
-            if (element == target) {
-                count++;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if (num == candidate) {
+                count = count + 1;
+            } else {
+                count = count - 1;
             }
         }
-        return count;
+
+        return candidate;
     }
 }
